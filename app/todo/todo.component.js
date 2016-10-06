48efe04383910eda08548a47af5bb60410dc59af
tmcothran <tmcothran@hotmail.com>
2016-10-06 16:45:57 -0400

    Have list of tasks tentatively printing out.

--- app/todo/todo.component.js
+++ app/todo/todo.component.js
@@ -1,11 +1,26 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+
+function Task(content) {
+    // A constructor function for tasks
+    this.content = content;
+    this.complete = false;
+}
+
+function toDoController() {
+    const self = this;
+    self.taskList = [];
+    self.taskInput = "And another";
+    self.taskList.push(
+        new Task(self.taskInput)
+    );
+}
+
+
 angular.module('todo').
-    component('testComponent', {
+    component('todoComponent', {
         templateUrl: 'todo/todo.template.html',
-        controller: function testController() {
-            this.testString = 'This is another test(string).';
-        }
+        controller: toDoController,
     });
 
