7d7767f9b05cff76b599904749f5776d9c414f8e
tmcothran <tmcothran@hotmail.com>
2016-10-07 13:45:52 -0400

    Currently, main todo component is broken.

--- app/todo/todo.component.js
+++ app/todo/todo.component.js
@@ -2,22 +2,41 @@
 
 // Declare app level module which depends on views, and components
 
-function toDoController() {
-    const self = this;
-    self.taskDisplayList = [];
-    self.taskInput = "And another";
-    self.taskDisplayList.push(
-        new Task(self.taskInput)
-    );
-    self.addTask = function(task) {
-        self.taskDisplayList.push(new Task(task));
+function toDoController(TaskList) {
+    var self = this;
+    self.taskList = function() {
+        return TaskList.list();
+    };
+    self.addTask = function() {
+        TaskList.add();
     };
 }
 
+function Task(content) {
+    // A constructor function for tasks
+    this.content = content;
+    this.complete = false;
+}
+
+function TaskList () {
+    var taskList = [];
+    return {
+        list: function() {
+            return taskList;
+        },
+        add: function(taskContent) {
+            taskList.push(
+                new Task(taskContent)
+            );
+        },
+    };
+}
 
 angular.module('todo').
-    component('todoComponent', {
+    component('todoComponent', [
+        'TaskList', {
         templateUrl: 'todo/todo.template.html',
-        controller: toDoController,
-    });
+        controller: toDoController(TaskList),
+        }
+    ]).factory('TaskList', TaskList);
 
