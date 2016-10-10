8cd3863493f65d9fceb76b2a05e31af099bfa8e7
tmcothran <tmcothran@hotmail.com>
2016-10-10 10:18:47 -0400

    Now can switch away from task component. List component is set up with empty files, it's not connected or working.

--- app.js
+++ app.js
@@ -3,5 +3,12 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'todo',
-]);
+]).controller('MainCtrl', [function() {
+    var self = this;
+    self.tab = 'tasks';
+    self.open = function(tab) {
+        self.tab = tab;
+    };
+}]);
     
+
