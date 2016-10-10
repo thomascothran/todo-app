43b1857e446f7802da7c5c50492da65e6a71b454
tmcothran <tmcothran@hotmail.com>
2016-10-10 10:51:58 -0400

    Refactored task list service into lists componenet.

--- list-manager/list-manager.component.js
+++ list-manager/list-manager.component.js
@@ -1,5 +1,59 @@
 'use strict';
 
+function Task(content) {
+    // A constructor function for tasks
+    var self = this;
+    self.content = content;
+    self.complete = false;
+    self.created = new Date();
+}
+
+function TaskList(name) {
+    // A constructor function for task lists
+    var self = this;
+    self.name = name;
+    self.created = new Date();
+    self.tasks = [];
+    self.add = function(taskContent) {
+        self.tasks.push(
+            new Task(taskContent)
+        );
+    };
+    self.remove = function(task) {
+        self.tasks = R.without(
+            [task], self.tasks
+        );
+    };
+    self.list = function() {
+        return self.tasks;
+    };
+}
+
+function TaskListService () {
+    // A service to persist task data
+    var self = this;
+    // Not use self?
+    self.taskLists = [];
+    // Add a default taskList
+    self.taskLists.push(
+        new TaskList('default')
+    ); 
+
+    return {
+        taskLists: function() {
+            return self.taskLists;
+        },
+        toggleComplete: function(task) {
+            if (task.complete) {
+                task.complete = false;
+                return task;
+            } else {
+                task.complete = true;
+                return task;
+            }
+        },
+    };
+}
 function listManagerComponent(TaskListService) {
     var self = this;
     self.test = 'This is a test';
@@ -10,4 +64,7 @@ angular.module('list-manager')
         templateUrl: 'list-manager/list-manager.template.html',
         controller: ['TaskListService', listManagerComponent]
     }
-);
+    ).
+    factory('TaskListService', [
+        TaskListService
+    ]);
--- todo/todo.component.js
+++ todo/todo.component.js
@@ -36,67 +36,11 @@ function toDoController(TaskListService) {
     };
 }
 
-function Task(content) {
-    // A constructor function for tasks
-    var self = this;
-    self.content = content;
-    self.complete = false;
-    self.created = new Date();
-}
-
-function TaskList(name) {
-    // A constructor function for task lists
-    var self = this;
-    self.name = name;
-    self.created = new Date();
-    self.tasks = [];
-    self.add = function(taskContent) {
-        self.tasks.push(
-            new Task(taskContent)
-        );
-    };
-    self.remove = function(task) {
-        self.tasks = R.without(
-            [task], self.tasks
-        );
-    };
-    self.list = function() {
-        return self.tasks;
-    };
-}
-
-function TaskListService () {
-    // A service to persist task data
-    var self = this;
-    // Not use self?
-    self.taskLists = [];
-    // Add a default taskList
-    self.taskLists.push(
-        new TaskList('default')
-    ); 
-
-    return {
-        taskLists: function() {
-            return self.taskLists;
-        },
-        toggleComplete: function(task) {
-            if (task.complete) {
-                task.complete = false;
-                return task;
-            } else {
-                task.complete = true;
-                return task;
-            }
-        },
-    };
-}
 
 angular.module('todo').
     component('todoComponent', {
         templateUrl: 'todo/todo.template.html',
         controller: ['TaskListService', toDoController,
         ]   
-    }).factory('TaskListService', [
-        TaskListService
-    ]);
+    });
 
